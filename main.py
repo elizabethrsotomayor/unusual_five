277bc1a6f02bbba9a0e760d1b8042ed1d89b7ad9
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-07-14 18:06:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def unusual_five():
+    """
+    Write a function that always returns 5
+
+    Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+    Good luck :)
+    :return:
+    """
+    lst = ["a", "b", "c", "d", "e"]
+    return len(lst)
